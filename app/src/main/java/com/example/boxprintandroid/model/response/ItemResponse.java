d4e1763e2e6af68184adf04330108e567134ae81
aldizzaldi <jeflam09@gmail.com>
2020-03-06 13:03:01 +0700

    modify little things

--- app/src/main/java/com/example/boxprintandroid/model/response/ItemResponse.java
+++ app/src/main/java/com/example/boxprintandroid/model/response/ItemResponse.java
@@ -1,5 +1,17 @@
 package com.example.boxprintandroid.model.response;
 
-public class ItemResponse {
+import com.example.boxprintandroid.pojo.Item;
+import com.google.gson.annotations.SerializedName;
 
+public class ItemResponse {
+    @SerializedName("item")
+    Item item;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 }
---
+++ app/src/main/java/com/example/boxprintandroid/pojo/Item.java
@@ -0,0 +1,60 @@
+package com.example.boxprintandroid.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Item {
+    @SerializedName("id")
+    int id;
+
+    @SerializedName("id_user")
+    int idUser;
+
+    @SerializedName("id_kategori")
+    int idKategori;
+
+    @SerializedName("nama_item")
+    String namaItem;
+
+    @SerializedName("deskripsi_item")
+    String deskripsiItem;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdKategori() {
+        return idKategori;
+    }
+
+    public void setIdKategori(int idKategori) {
+        this.idKategori = idKategori;
+    }
+
+    public String getNamaItem() {
+        return namaItem;
+    }
+
+    public void setNamaItem(String namaItem) {
+        this.namaItem = namaItem;
+    }
+
+    public String getDeskripsiItem() {
+        return deskripsiItem;
+    }
+
+    public void setDeskripsiItem(String deskripsiItem) {
+        this.deskripsiItem = deskripsiItem;
+    }
+}
