8755dbacf21251d5fb21e6d3f37a2450a802c9da
mhdellamh <44257183+mhdellamh@users.noreply.github.com>
2020-03-09 11:42:12 +0700

    nothing

--- app/src/main/java/com/example/boxprintandroid/ui/dumies_product/MainActivity.java
+++ app/src/main/java/com/example/boxprintandroid/ui/dumies_product/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_product_list);
+        setContentView(R.layout.activity_list_product);
 
         rvItem = findViewById(R.id.rv_item);
         rvItem.setHasFixedSize(true);
