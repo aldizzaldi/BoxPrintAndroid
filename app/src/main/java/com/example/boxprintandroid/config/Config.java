ed7c3570c09aa4c6cff9d25e7a91683c85932248
aldizzaldi <jeflam09@gmail.com>
2020-03-12 13:55:37 +0700

    add paypal

---
+++ app/src/main/java/com/example/boxprintandroid/config/Config.java
@@ -0,0 +1,5 @@
+package com.example.boxprintandroid.config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AVvbBZH4NBOz1nMQObUvQvJr7yp8NockL8jkYnM4QUR56spZ5WNelwtUAjRGkFE06NTpDwLXqNcgjQDt";
+}
